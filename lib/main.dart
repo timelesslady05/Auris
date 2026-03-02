ee67d71a57a1545b88082a3e7cd0107d4748bc19
timelesslady05 <joseph.sarahsc05@gmail.com>
2026-03-02 09:06:54 +0530

    Initialize Firebase and configure Android Gradle

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(AurisApp());
 }
 
